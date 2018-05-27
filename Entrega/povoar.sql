bbdea4fd211256ebda4a8307bf2a90fdfe60bfe9
neto22 <pneto.98@gmail.com>
2018-05-27 17:05:04 +0100

    parte errada comentada

--- Entrega/povoar.sql
+++ Entrega/povoar.sql
@@ -12,7 +12,7 @@ INSERT INTO "Entidade" VALUES (99999, 'Events4U', 931111111,'Avenida dos Aliados
 INSERT INTO "Entidade" VALUES (95555, 'Best Show Agency', 931111111,'Avenida da Boavista 55 Porto');
 
 
-/*ENTIDADES NOVAS */
+/*ENTIDADES NOVAS
 INSERT INTO "Entidade" VALUES (81523, 'Festas Bonitas', 924556321,'Rua das Flores 27 Porto');
 INSERT INTO "Entidade" VALUES (54268, 'FeiraFesta', 924553321,'Via St. Inacio 31 2oC Madalena');
 INSERT INTO "Entidade" VALUES (12585, 'Rural Festival', 911863631,'Rua Dr. Ramalhao Faria 501 Vila Nova de Gaia');
@@ -47,6 +47,7 @@ INSERT INTO "Entidade" VALUES (77777, 'Isidoro', 921111111,'Avenida Joao Pereira
 INSERT INTO "Entidade" VALUES (72727, 'Catarina', 921111111,'Rua de Sao Marcal 77 1249-003 Aveiro');
 INSERT INTO "Entidade" VALUES (96399, 'Rita', 921111111,'Rua de Sao Domingos à Lapa 26 1249-003 Moreira da Maia');
 INSERT INTO "Entidade" VALUES (10005, 'Isabel', 921111111,'Travessa do Carvalho 23 1249-003 Setubal');
+*/
 /* */
 
 
@@ -55,7 +56,7 @@ INSERT INTO "Cliente" VALUES (55555, 2);
 INSERT INTO "Cliente" VALUES (99999, 3);
 INSERT INTO "Cliente" VALUES (95555, 4);
 
-/*CLIENTES NOVOS*/
+/*CLIENTES NOVOS
 INSERT INTO "Cliente" VALUES (23321, 5);
 INSERT INTO "Cliente" VALUES (80085, 6);
 INSERT INTO "Cliente" VALUES (41367, 7);
@@ -67,6 +68,7 @@ INSERT INTO "Cliente" VALUES (12305, 12);
 INSERT INTO "Cliente" VALUES (64200, 13);
 INSERT INTO "Cliente" VALUES (98907, 14);
 INSERT INTO "Cliente" VALUES (20005, 15);
+*/
 /* */
 
 /* Adicionei novos postos de trabalho */
@@ -79,7 +81,7 @@ INSERT INTO "Empregado" VALUES (12346, "Gerente", NULL);
 INSERT INTO "Empregado" VALUES (12345, "Empregado de Loja", 12346);
 INSERT INTO "Empregado" VALUES (15555, "Empregado de Loja", 12346);
 
-/*EMPREGADOS NOVOS*/
+/*EMPREGADOS NOVOS
 INSERT INTO "Empregado" VALUES (19059, "Gestor de Armazem", 12346);
 INSERT INTO "Empregado" VALUES (77777, "Gestor de Armazem", 12346);
 INSERT INTO "Empregado" VALUES (51198, "Empregado de Loja", 12346);
@@ -89,12 +91,13 @@ INSERT INTO "Empregado" VALUES (53888, "Empregado de Armazem", 77777);
 INSERT INTO "Empregado" VALUES (72727, "Empregado de Armazem", 77777);
 INSERT INTO "Empregado" VALUES (10005, "Empregado de Armazem", 19059);
 INSERT INTO "Empregado" VALUES (96399, "Empregado de Armazem", 19059);
+*/
 /* */
 
 INSERT INTO "OrganizadorEventos" VALUES (99999, "Desconto de 15% em instrumentos de sopro");
 INSERT INTO "OrganizadorEventos" VALUES (95555, NULL);
 
-/*ORGANIZADORES NOVOS*/
+/*ORGANIZADORES NOVOS
 INSERT INTO "OrganizadorEventos" VALUES (81523, "Desconto de 10% em todos os instrumentos");
 INSERT INTO "OrganizadorEventos" VALUES (54268, "Desconto de 25% em instrumentos de percursao");
 INSERT INTO "OrganizadorEventos" VALUES (12585, "Desconto de 35% em acessorios");
@@ -107,12 +110,13 @@ INSERT INTO "OrganizadorEventos" VALUES (64863, NULL);
 INSERT INTO "OrganizadorEventos" VALUES (76185, NULL);
 INSERT INTO "OrganizadorEventos" VALUES (44132, "Desconto de 25% em palhetas");
 INSERT INTO "OrganizadorEventos" VALUES (47235, NULL);
+*/
 /* */
 
 INSERT INTO "Sala" VALUES ("Guilhermina Suggia", "Casa da Musica, Rotunda da Boavista 45 Porto");
 INSERT INTO "Sala" VALUES ("Hard Club", "Rua Monzinho da Silveira 45 Porto");
 
-/*SALAS NOVAS*/
+/*SALAS NOVAS
 INSERT INTO "Sala" VALUES ("Maus Habitos", "Rua de Passos Manuel 178, 4º Piso");
 INSERT INTO "Sala" VALUES ("Bar do Alex", "Avenida Saraiva de Carvalho 3084-501 Figueira da Foz");
 INSERT INTO "Sala" VALUES ("Museu de Moedas Antigas", "Rua Dr. Cristiano Salah 45 Ribeira Grande");
@@ -123,12 +127,13 @@ INSERT INTO "Sala" VALUES ("Teatro Sá da Bandeira", "Rua de Sá da Bandeira 108
 INSERT INTO "Sala" VALUES ("Mar Shopping", "Avenida Dr. Óscar Lopes, 4450-337 Leca da Palmeira");
 INSERT INTO "Sala" VALUES ("Torre dos Clerigos", "Rua de Sao Filipe de Nery, 4050-546 Porto");
 INSERT INTO "Sala" VALUES ("Pavilhao Rosa Mota", "Rua de Dom Manuel II, 4050-346 Porto");
+*/
 /* */
 
 INSERT INTO "MarcouSala" VALUES (99999, "Hard Club");
 INSERT INTO "MarcouSala" VALUES (95555, "Guilhermina Suggia");
 
-/*MARCACOES NOVAS*/
+/*MARCACOES NOVAS
 INSERT INTO "MarcouSala" VALUES (81523, "Bar do Alex");
 INSERT INTO "MarcouSala" VALUES (12585, "Coliseu do Porto");
 INSERT INTO "MarcouSala" VALUES (55555, "Teatro Sá da Bandeira");
@@ -140,6 +145,7 @@ INSERT INTO "MarcouSala" VALUES (99723, "Hard Club");
 INSERT INTO "MarcouSala" VALUES (47235, "Pavilhao Rosa Mota");
 INSERT INTO "MarcouSala" VALUES (64863, "Teatro Sá da Bandeira");
 INSERT INTO "MarcouSala" VALUES (76185, "Estadio do Dragao");
+*/
 /* */
 
 INSERT INTO "Aluguer" VALUES (1, "2018-04-13", "2018-04-15", 52345, NULL);
@@ -147,7 +153,7 @@ INSERT INTO "Aluguer" VALUES (2, "2018-04-12", "2018-04-18", 52345, NULL);
 INSERT INTO "Aluguer" VALUES (3, "2018-04-13", "2018-04-15", NULL, "Hard Club");
 INSERT INTO "Aluguer" VALUES (4, "2018-04-12", "2018-04-19", 99999, NULL);
 
-/*ALUGUERES NOVOS*/
+/*ALUGUERES NOVOS
 INSERT INTO "Aluguer" VALUES (5, "2018-05-12", "2018-05-18", 80085, NULL);
 INSERT INTO "Aluguer" VALUES (6, "2018-04-18", "2018-04-28", 23321, NULL);
 --INSERT INTO "Aluguer" VALUES (7, "2018-05-12", "2018-04-19", 63219, NULL);
@@ -159,6 +165,7 @@ INSERT INTO "Aluguer" VALUES (12, "2018-04-29", "2018-05-03", NULL, "Bar do Alex
 INSERT INTO "Aluguer" VALUES (13, "2018-05-24", "2018-06-04", NULL, "Pavilhao Rosa Mota");
 INSERT INTO "Aluguer" VALUES (14, "2018-04-03", "2018-04-18", NULL, "Mar Shopping");
 INSERT INTO "Aluguer" VALUES (15, "2018-05-19", "2018-06-08", NULL, "Coliseu do Porto");
+*/
 /* */
 
 INSERT INTO "Marca" VALUES ("Gibson");
@@ -166,7 +173,7 @@ INSERT INTO "Marca" VALUES ("Yamaha");
 INSERT INTO "Marca" VALUES ("Fender");
 INSERT INTO "Marca" VALUES ("Pearl");
 
-/*MARCAS NOVAS */
+/*MARCAS NOVAS
 INSERT INTO "Marca" VALUES ("KORG");
 INSERT INTO "Marca" VALUES ("Cort");
 INSERT INTO "Marca" VALUES ("Roland");
@@ -174,6 +181,7 @@ INSERT INTO "Marca" VALUES ("D'Addario");
 INSERT INTO "Marca" VALUES ("Hohner");
 INSERT INTO "Marca" VALUES ("Cecilio");
 INSERT INTO "Marca" VALUES ("Cremona");
+*/
 /* */
 
 INSERT INTO "QualidadeModelo" VALUES ("Recem-Comprada");
@@ -187,7 +195,7 @@ INSERT INTO "Modelo" VALUES ("DX4", 1000, NULL, "Bateria", "Pearl");
 INSERT INTO "Modelo" VALUES ("Telecaster", 1000, "Acer", "Guitarra Eletrica", "Fender");
 INSERT INTO "Modelo" VALUES ("Stratocaster", 1000, "Acer", "Guitarra Eletrica", "Fender");
 
-/*MODELOS NOVOS*/
+/*MODELOS NOVOS
 INSERT INTO "Modelo" VALUES ("SV-1", 3000, "Metal", "Teclado Sintetizador", "KORG");
 INSERT INTO "Modelo" VALUES ("Saturn 09", 1500, "Plastico", "Teclado Sintetizador", "Roland");
 INSERT INTO "Modelo" VALUES ("Jupiter 8", 6500, "Mogno", "Teclado Sintetizador", "Roland");
@@ -197,6 +205,7 @@ INSERT INTO "Modelo" VALUES ("Progressive", 80, "Varios", "Harmonica", "Hohner")
 INSERT INTO "Modelo" VALUES ("Marine Band", 25, "Varios", "Harmonica", "Hohner");
 INSERT INTO "Modelo" VALUES ("Beginner Road", 600, "Plastico", "Violoncelo", "Cecilio");
 INSERT INTO "Modelo" VALUES ("CX2", 2500, "Mogno", "Violoncelo", "Cremona");
+*/
 /* */
 
 INSERT INTO "PrecoAluguer" VALUES ("Les Paul", "Aceitavel", 16);
@@ -205,7 +214,7 @@ INSERT INTO "PrecoAluguer" VALUES ("DX4", "Aceitavel", 14);
 INSERT INTO "PrecoAluguer" VALUES ("Telecaster", "Impecavel", 18);
 INSERT INTO "PrecoAluguer" VALUES ("Pacifica", "Recem-Comprada", 18);
 
-/*PREÇO ALUGUER NOVOS*/
+/*PREÇO ALUGUER NOVOS
 INSERT INTO "PrecoAluguer" VALUES ("SV-1", "Aceitavel", 25);
 INSERT INTO "PrecoAluguer" VALUES ("Saturn 09", "Gasto", 10);
 INSERT INTO "PrecoAluguer" VALUES ("Jupiter 8", "Recem-Comprada", 60);
@@ -215,6 +224,7 @@ INSERT INTO "PrecoAluguer" VALUES ("Progressive", "Gasto", 8);
 INSERT INTO "PrecoAluguer" VALUES ("Marine Band", "Impecavel", 4);
 INSERT INTO "PrecoAluguer" VALUES ("Beginner Road", "Gasto", 15);
 INSERT INTO "PrecoAluguer" VALUES ("CX2", "Recem-Comprada", 35);
+*/
 /* */
 
 INSERT INTO "Instrumento" VALUES (1, "Recem-Comprada", "Pacifica", NULL);
@@ -223,7 +233,7 @@ INSERT INTO "Instrumento" VALUES (3, "Impecavel", "Telecaster", 2);
 INSERT INTO "Instrumento" VALUES (4, "Aceitavel", "DX4", 3);
 INSERT INTO "Instrumento" VALUES (5, "Gasto", "Les Paul", 4);
 
-/*INSTRUMENTOS NOVOS*/
+/*INSTRUMENTOS NOVOS
 INSERT INTO "Instrumento" VALUES (6, "Recem-Comprada", "Jupiter 8", 5);
 INSERT INTO "Instrumento" VALUES (7, "Gasto", "Saturn 09", 7);
 INSERT INTO "Instrumento" VALUES (8, "Aceitavel", "SV-1", 8);
@@ -238,13 +248,15 @@ INSERT INTO "Instrumento" VALUES (16, "Impecavel", "Marine Band", 14);
 INSERT INTO "Instrumento" VALUES (17, "Gasto", "Progressive", 14);
 INSERT INTO "Instrumento" VALUES (18, "Impecavel", "DA-X5", 11);
 INSERT INTO "Instrumento" VALUES (19, "Gasto", "Progressive", 6);
+*/
 /* */
 
 INSERT INTO "Loja" VALUES ("NorteShopping", 150);
 INSERT INTO "Loja" VALUES ("Boavista", 250);
-/*LOJAS NOVAS */
+/*LOJAS NOVAS 
 INSERT INTO "Loja" VALUES ("Vivaci", 200);
 INSERT INTO "Loja" VALUES ("Zona Industrial da Maia", 100);
+*/
 
 INSERT INTO "EmLoja" VALUES ("Pacifica", "NorteShopping", 3);
 INSERT INTO "EmLoja" VALUES ("Les Paul", "NorteShopping", 1);
@@ -252,7 +264,7 @@ INSERT INTO "EmLoja" VALUES ("Pacifica", "Boavista", 2);
 INSERT INTO "EmLoja" VALUES ("DX4", "NorteShopping", 1);
 INSERT INTO "EmLoja" VALUES ("Telecaster", "Boavista", 3);
 
-/*EMLOJA NOVAS */
+/*EMLOJA NOVAS
 INSERT INTO "EmLoja" VALUES("SV-1","Vivaci",1);
 INSERT INTO "EmLoja" VALUES("SV-1","Zona Industrial da Maia",2);
 INSERT INTO "EmLoja" VALUES("Marine Band","NorteShopping",3);
@@ -263,12 +275,13 @@ INSERT INTO "EmLoja" VALUES("Saturn 09","Zona Industrial da Maia",1);
 INSERT INTO "EmLoja" VALUES("Progressive","Vivaci",1);
 INSERT INTO "EmLoja" VALUES("Progressive","Boavista", 2);
 INSERT INTO "EmLoja" VALUES("AD810","Vivaci", 3);
+*/
 
 INSERT INTO "EmpregadosDaLoja" VALUES ("NorteShopping", 12345);
 INSERT INTO "EmpregadosDaLoja" VALUES ("NorteShopping", 12346);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Boavista", 15555);
 
-/*EMPREGADOSDALOJA NOVAS */
+/*EMPREGADOSDALOJA NOVAS 
 INSERT INTO "EmpregadosDaLoja" VALUES ("Zona Industrial da Maia", 77777);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Zona Industrial da Maia", 53888);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Zona Industrial da Maia", 10005);
@@ -278,3 +291,4 @@ INSERT INTO "EmpregadosDaLoja" VALUES ("Vivaci", 72727);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Vivaci", 96399);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Vivaci", 51198);
 INSERT INTO "EmpregadosDaLoja" VALUES ("Vivaci", 19198);
+*/
